4ff5bbe9af118a16342c07dfefc8ad47e20db066
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-11-10 14:00:21 +0700

    obj and arr

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+// BASICS
 function add(n1: number, n2: number, sr: boolean, rp: string) {
   if (sr) {
     console.log(`${rp} ${n1 + n2}`);
@@ -12,3 +13,38 @@ const showRes = true;
 const phrase = "The Resuls is: ";
 
 add(num1, num2, showRes, phrase);
+
+// OBJECTS
+const country: {
+  name: string;
+  independence: string;
+  totalIsland: number;
+  president: string[];
+  javaIsland: string[];
+} = {
+  name: "Indonesia",
+  independence: "August 17, 1945",
+  totalIsland: 17000,
+  president: ["Soekarnoe", "Soehartoe"],
+  javaIsland: ["East Java", "Center Java", "West Java"],
+};
+
+console.log(country);
+
+// Array
+let favoriteSport: string[];
+favoriteSport = ["Football", "Volleyball"];
+
+let mixedArr: any[];
+mixedArr = [1, "Two", true];
+
+console.log(favoriteSport);
+console.log(mixedArr);
+
+for (const president of country.president) {
+  console.log(president);
+}
+
+country.javaIsland.forEach((island) => {
+  console.log(island);
+});
